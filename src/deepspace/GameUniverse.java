8291df9c3c3e6cf9439f03afbf0bf7c3f86d6a31
PedroPabloRH <ppruiz98@gmail.com>
2018-03-20 23:19:42 +0100

    Clase GameUniverse comenzada.

---
+++ src/deepspace/GameUniverse.java
@@ -0,0 +1,64 @@
+
+
+package deepspace;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Pedro Pablo Ruiz Huertas y Juan Antonio Villegas Recio
+ */
+public class GameUniverse {
+    private int WIN=10;
+    private int currentStationIndex;
+    private int turns;
+    private GameStateController gamestate;
+    private EnemyStarShip currentEnemy;
+    private Dice dice;
+    private SpaceStation currentStation;
+    private ArrayList<SpaceStation> SpaceStations;
+    
+    public GameUniverse(){
+        this.turns=0;
+        this.dice=new Dice();
+        gamestate=new GameStateController();
+    }
+    
+    boolean haveAWinner(){
+        if(currentStation.getNMedals()==WIN){
+            return true;
+        }
+        else return false;
+    }
+    
+    void discardHangar(){
+        currentStation.discardHangar();
+    }
+    
+    void discardShieldBooster(int i){
+        currentStation.discardShieldBooster(i);
+    }
+    
+    void discardShieldBoosterInHangar(int i){
+        currentStation.discardShieldBoosterInHangar(i);
+    }
+    
+    void discardWeapon(int i){
+        currentStation.discardWeapon(i);
+    }
+    
+    void discardWeaponInHangar(int i){
+        currentStation.discardWeaponInHangar(i);
+    }
+    
+    GameState getState(){
+        return this.gamestate.getState();
+    }
+    
+    GameUniverseToUI getUIversion(){
+        return new GameUniverseToUI(this.currentStation, this.currentEnemy);
+    }
+    
+    
+    
+}
