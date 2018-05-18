7969e553dc52cc2af5f46f124d0db0df3b4f73f9
JuanAntonio <juanantoniovr98@gmail.com>
2018-05-18 13:26:47 +0200

    Clase Interfaz

---
+++ src/controller/ControllerGraph.java
@@ -0,0 +1,129 @@
+
+
+package controller;
+
+
+import View.View;
+import java.util.ArrayList;
+import deepspace.GameState;
+import deepspace.GameUniverse;
+import deepspace.GameUniverseToUI;
+import deepspace.Weapon;
+import deepspace.ShieldBooster;
+import deepspace.WeaponToUI;
+import deepspace.ShieldToUI;
+import deepspace.CombatResult;
+
+
+
+/**
+ *
+ * @author Pedro Pablo Ruiz Huertas y Juan Antonio Villegas Recio
+ */
+public class ControllerGraph {
+    private GameUniverse model;
+    private View view;
+    
+    public ControllerGraph(GameUniverse m, View v) {
+        model = m;
+        view = v;
+        view.setController(this);
+    }
+    
+    public void start() {
+        model.init(view.getNames());
+        view.updateView();
+        view.showView();
+    }
+    
+    public void finish(int i) {
+        if (view.confirmExitMessage())
+            System.exit(i);
+    }
+    
+    public GameState getGameState () {
+        return model.getState();
+    }
+    
+    public GameUniverseToUI getModelToUI () {
+        return model.getUIversion();
+    }
+    //=================================================================
+    static public enum Element {
+        WEAPON ("Arma"), 
+        SHIELD ("Escudo"), 
+        HANGAR ("Hangar");
+
+        final public String text;
+
+        Element (String t) { text = t; }
+    };
+
+    static public enum Operation {
+        MOUNT ("Montar"), 
+        DISCARD ("Descartar");
+
+        final public String text;
+
+        Operation (String t) { text = t; }
+    };
+    //==================================================================
+    
+    public CombatResult combat() {
+        CombatResult result = model.combat();
+        view.updateView();
+        return result;
+    }
+    
+    public boolean haveAWinner() {
+        return model.haveAWinner();
+    }
+
+    public void discardHangar() {
+        model.discardHangar();
+        view.updateView();
+    }
+
+    public boolean nextTurn() {
+        boolean result = model.nextTurn();
+        view.updateView();
+        return result;
+    }
+
+    public void mountWeapon(int option) {
+        model.mountWeapon(option);
+        view.updateView();
+    }
+    
+    public void mountDiscardFromHangar(Operation operation, Element element, int option) {
+        switch (element) {
+            case WEAPON :
+              if (operation == Operation.MOUNT)
+                  model.mountWeapon(option);
+              else
+                  model.discardWeaponInHangar(option);
+              break;
+            case SHIELD :
+              if (operation == Operation.MOUNT)
+                  model.mountShieldBooster(option);
+              else
+                  model.discardShieldBoosterInHangar(option);
+              break;
+        }
+        view.updateView();
+    }
+    
+    public void discardMountedElements(Element element, int option) {
+        switch (element) {
+            case WEAPON :
+              model.discardWeapon(option);
+              break;
+            case SHIELD :
+              model.discardShieldBooster(option);
+              break;
+        }
+        view.updateView();
+        
+    }
+    
+}
