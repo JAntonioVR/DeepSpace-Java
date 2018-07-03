195de216ecc27d01b646188da09eb00254284ae2
PedroPabloRH <ppruiz98@gmail.com>
2018-07-03 20:05:44 +0200

    Añandido el caso donde la estación se transforma.

--- src/UI/TextUI.java
+++ src/UI/TextUI.java
@@ -75,6 +75,13 @@ public class TextUI {
                     System.exit (0);
                 }
                 break;
+                case STATIONWINSANDCONVERTS :
+                pause ("\n Has GANADO el combate. Disfruta de tu botín. Y, además, ¡¡¡¡te has transformado.!!!!");
+                if (controller.haveAWinner()) {
+                    pause ("\n\n **** **** ****  HAS GANADO LA PARTIDA  **** **** ****\n");
+                    System.exit (0);
+                }
+                break;
           }
           do {   // Until a valid next turn
               System.out.println (showStation (gameUI.getCurrentStation()));
